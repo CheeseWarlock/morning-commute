09ae14b91ca3d4d36071eeed0db8f2d2498a6e20
Joe <joe.cardwell@gmail.com>
2023-08-09 16:24:12 -0400

    Add tests for straight line segments

--- src/engine/TrackSegment.ts
+++ src/engine/TrackSegment.ts
@@ -2,17 +2,23 @@ import Point from "./Point";
 
 class TrackSegment {
     length: number;
-    #start: Point;
-    #end: Point;
+    start: Point;
+    end: Point;
     
     constructor(start: Point, end: Point) {
-        this.#start = start;
-        this.#end = end;
+        this.start = start;
+        this.end = end;
         this.length = Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2);
     }
 
-    getDistanceAlong(distance: number) {
-        return 0;
+    getPositionAlong(distance: number): Point {
+        const dx = (this.end.x - this.start.x) / this.length;
+        const dy = (this.end.y - this.start.y) / this.length;
+
+        return {
+            x: this.start.x + dx * distance,
+            y: this.start.y + dy * distance
+        };
     }
 }
 
--- src/engine/tests/TrackSegment.test.ts
+++ src/engine/tests/TrackSegment.test.ts
@@ -1,14 +1,25 @@
 import TrackSegment from "../TrackSegment";
 
-describe('TrackSegment', () => {
-    it('should construct a track segment', () => {
-        const pointA = { x: 0, y: 0 };
-        const pointB = { x: 1, y: 1 };
+describe('a single TrackSegment', () => {
+    const pointA = { x: 0, y: 0 };
+    const pointB = { x: 10, y: 0 };
+    const pointC = { x: 10, y: 10 };
 
-        const segment = new TrackSegment(pointA, pointB);
+    it('should construct a track segment', () => {
+        new TrackSegment(pointA, pointB);
     });
 
-    it('should calculate distance along a track segment', () => {
+    it('should calculate distance along a track segment for a y=0 line', () => {
+        const segment = new TrackSegment(pointA, pointB);
+        const positionAlong = segment.getPositionAlong(1);
+        expect(positionAlong.x).toBe(1);
+        expect(positionAlong.y).toBe(0);
+    });
 
+    it('should calculate distance along a track segment for a diagonal line', () => {
+        const segment = new TrackSegment(pointA, pointC);
+        const positionAlong = segment.getPositionAlong(Math.sqrt(2));
+        expect(positionAlong.x).toBeCloseTo(1, 4);
+        expect(positionAlong.y).toBeCloseTo(1, 4);
     });
 });
