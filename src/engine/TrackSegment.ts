e67ede77e064f7f3c430217f7218a76cbb5734d8
Joe <joe.cardwell@gmail.com>
2023-08-09 16:38:44 -0400

    Add Prettier, and better distanceAlong function

--- src/engine/TrackSegment.ts
+++ src/engine/TrackSegment.ts
@@ -1,25 +1,46 @@
 import Point from "./Point";
 
 class TrackSegment {
-    length: number;
-    start: Point;
-    end: Point;
-    
-    constructor(start: Point, end: Point) {
-        this.start = start;
-        this.end = end;
-        this.length = Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2);
+  length: number;
+  start: Point;
+  end: Point;
+  atStart: TrackSegment[];
+  atEnd: TrackSegment[];
+
+  constructor(start: Point, end: Point) {
+    this.start = start;
+    this.end = end;
+    this.length = Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2);
+    this.atStart = [];
+    this.atEnd = [];
+  }
+
+  get isWellConnected() {
+    return this.atStart?.length > 0 && this.atEnd.length > 0;
+  }
+
+  getPositionAlong(distance: number): { point: Point; excess: number } {
+    const dx = (this.end.x - this.start.x) / this.length;
+    const dy = (this.end.y - this.start.y) / this.length;
+
+    if (distance > this.length) {
+      return {
+        point: {
+          x: this.end.x,
+          y: this.end.y,
+        },
+        excess: distance - this.length,
+      };
     }
 
-    getPositionAlong(distance: number): Point {
-        const dx = (this.end.x - this.start.x) / this.length;
-        const dy = (this.end.y - this.start.y) / this.length;
-
-        return {
-            x: this.start.x + dx * distance,
-            y: this.start.y + dy * distance
-        };
-    }
+    return {
+      point: {
+        x: this.start.x + dx * distance,
+        y: this.start.y + dy * distance,
+      },
+      excess: 0,
+    };
+  }
 }
 
 export default TrackSegment;
--- src/engine/tests/TrackSegment.test.ts
+++ src/engine/tests/TrackSegment.test.ts
@@ -1,25 +1,44 @@
 import TrackSegment from "../TrackSegment";
 
-describe('a single TrackSegment', () => {
-    const pointA = { x: 0, y: 0 };
-    const pointB = { x: 10, y: 0 };
-    const pointC = { x: 10, y: 10 };
+describe("a single TrackSegment", () => {
+  const pointA = { x: 0, y: 0 };
+  const pointB = { x: 10, y: 0 };
+  const pointC = { x: 10, y: 10 };
 
-    it('should construct a track segment', () => {
-        new TrackSegment(pointA, pointB);
-    });
+  it("should construct a track segment", () => {
+    new TrackSegment(pointA, pointB);
+  });
 
-    it('should calculate distance along a track segment for a y=0 line', () => {
-        const segment = new TrackSegment(pointA, pointB);
-        const positionAlong = segment.getPositionAlong(1);
-        expect(positionAlong.x).toBe(1);
-        expect(positionAlong.y).toBe(0);
-    });
+  it("should calculate distance along a track segment for a y=0 line", () => {
+    const segment = new TrackSegment(pointA, pointB);
+    const positionAlong = segment.getPositionAlong(1);
+    expect(positionAlong.point.x).toBe(1);
+    expect(positionAlong.point.y).toBe(0);
+  });
 
-    it('should calculate distance along a track segment for a diagonal line', () => {
-        const segment = new TrackSegment(pointA, pointC);
-        const positionAlong = segment.getPositionAlong(Math.sqrt(2));
-        expect(positionAlong.x).toBeCloseTo(1, 4);
-        expect(positionAlong.y).toBeCloseTo(1, 4);
-    });
+  it("should calculate distance along a track segment for a diagonal line", () => {
+    const segment = new TrackSegment(pointA, pointC);
+    const positionAlong = segment.getPositionAlong(Math.sqrt(2));
+    expect(positionAlong.point.x).toBeCloseTo(1, 4);
+    expect(positionAlong.point.y).toBeCloseTo(1, 4);
+  });
+});
+
+describe("a series of TrackSegments", () => {
+  const pointA = { x: 0, y: 0 };
+  const pointB = { x: 10, y: 0 };
+  const pointC = { x: 10, y: 10 };
+
+  it("should determine excess distance when applicable", () => {
+    const segmentA = new TrackSegment(pointA, pointB);
+    const segmentB = new TrackSegment(pointB, pointC);
+
+    segmentA.atEnd.push(segmentB);
+    segmentB.atStart.push(segmentA);
+
+    const positionAlong = segmentA.getPositionAlong(15);
+    expect(positionAlong.point.x).toBeCloseTo(10, 4);
+    expect(positionAlong.point.y).toBeCloseTo(0, 4);
+    expect(positionAlong.excess).toBeCloseTo(5, 4);
+  });
 });
