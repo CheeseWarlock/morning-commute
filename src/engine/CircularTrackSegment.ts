a520a97b67d457d513a61a306997a40c9130a04c
Joe <joe.cardwell@gmail.com>
2023-08-10 12:11:02 -0400

    TDD for position along circular track

--- src/engine/CircularTrackSegment.ts
+++ src/engine/CircularTrackSegment.ts
@@ -9,6 +9,10 @@ class CircularTrackSegment implements TrackSegment {
   center: Point;
   counterClockWise: boolean;
   _length: number;
+  #theta: number;
+  #radius: number;
+  #initialAngle: number;
+  #finalAngle: number;
 
   constructor(
     start: Point,
@@ -29,6 +33,8 @@ class CircularTrackSegment implements TrackSegment {
       this.end.y - this.center.y,
       this.end.x - this.center.x,
     );
+    this.#initialAngle = initialAngle;
+    this.#finalAngle = finalAngle;
 
     // Convert angles to range of [0 - 2*Pi]
     initialAngle = (initialAngle + 2 * Math.PI) % (2 * Math.PI);
@@ -49,6 +55,8 @@ class CircularTrackSegment implements TrackSegment {
     const radius = Math.sqrt(
       (center.x - start.x) ** 2 + (center.y - start.y) ** 2,
     );
+    this.#theta = Math.abs(finalAngle - initialAngle);
+    this.#radius = radius;
     this._length = Math.abs(finalAngle - initialAngle) * radius;
     this.atStart = [];
     this.atEnd = [];
@@ -75,10 +83,29 @@ class CircularTrackSegment implements TrackSegment {
     distance: number,
     reverse: boolean = false,
   ): { point: Point; excess: number } {
+    if (distance > this.length) {
+      return {
+        point: {
+          x: this.end.x,
+          y: this.end.y,
+        },
+        excess: distance - this.length,
+      };
+    }
+
+    // theta = L / r
+    const theta = distance / this.#radius;
+    let angleAlong = 0;
+    if (this.counterClockWise) {
+      angleAlong = this.#initialAngle - theta;
+    } else {
+      angleAlong = this.#initialAngle + theta;
+    }
+
     return {
       point: {
-        x: 0,
-        y: 0,
+        x: this.center.x + Math.cos(angleAlong),
+        y: this.center.y + Math.sin(angleAlong),
       },
       excess: 0,
     };
--- src/engine/tests/CircularTrackSegment.test.ts
+++ src/engine/tests/CircularTrackSegment.test.ts
@@ -14,4 +14,21 @@ describe("a single CircularTrackSegment", () => {
     const segment = new CircularTrackSegment(pointA, pointC, pointB, false);
     expect(segment.length).toBeCloseTo((3 * (10 * Math.PI)) / 2, 4);
   });
+
+  it("should report the end point when distance exceeds length", () => {
+    const segment = new CircularTrackSegment(pointA, pointC, pointB, true);
+    const positionAlong = segment.getPositionAlong(100);
+    expect(positionAlong.point.x).toBe(10);
+    expect(positionAlong.point.y).toBe(10);
+    expect(positionAlong.excess).toBe(100 - segment.length);
+  });
+
+  it("should report the position along the track when distance is less than length", () => {
+    const segment = new CircularTrackSegment(pointA, pointC, pointB, true);
+    const positionAlong = segment.getPositionAlong(segment.length / 2);
+    const v = Math.cos(Math.PI / 4) * 10;
+    expect(positionAlong.point.x).toBeCloseTo(10 - v, 4);
+    expect(positionAlong.point.y).toBeCloseTo(v, 4);
+    expect(positionAlong.excess).toBe(0);
+  });
 });
