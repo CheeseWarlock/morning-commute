fd9fe2fa75795db17751e9109b281956e1c93303
Joe <joe.cardwell@gmail.com>
2023-08-11 15:55:36 -0400

    Improve visualization

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,26 @@ import Map from "./renderer/basic/map.ts";
 import LinearTrackSegment from "./engine/LinearTrackSegment.ts";
 import CircularTrackSegment from "./engine/CircularTrackSegment.ts";
 
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//     <p id="map-holder"></p>
+//   </div>
+// `;
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
     <p id="map-holder"></p>
   </div>
 `;
@@ -61,5 +66,3 @@ q.connect(v2);
 q.connect(v4);
 const network = [h1, h2, h3, v1, v2, v3, v4, q, q2];
 const map = new Map(document.querySelector("#map-holder")!, network);
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
--- src/renderer/basic/map.ts
+++ src/renderer/basic/map.ts
@@ -40,8 +40,22 @@ class Map {
   #canvas: HTMLCanvasElement;
   #network: TrackSegment[];
   trains: Train[];
-  constructor(element: HTMLElement, network: TrackSegment[]) {
+  #offset: { x: number; y: number };
+  #size: { x: number; y: number };
+  #scale: number;
+  constructor(
+    element: HTMLElement,
+    network: TrackSegment[],
+    offset = { x: 10, y: 10 },
+    scale = 2,
+    size = { x: 250, y: 250 },
+  ) {
+    this.#offset = offset;
+    this.#scale = scale;
+    this.#size = size;
     const canvas = document.createElement("canvas");
+    canvas.width = size.x;
+    canvas.height = size.y;
     element.appendChild(canvas);
     this.#canvas = canvas;
     this.#network = network;
@@ -64,20 +78,26 @@ class Map {
     if (!context) return;
 
     // Render
-    context.clearRect(0, 0, 250, 150);
+    context.clearRect(0, 0, this.#size.x, this.#size.y);
     context.strokeStyle = "rgb(200, 200, 200)";
     this.#network.forEach((segment) => {
       if (segment instanceof LinearTrackSegment) {
         context.beginPath();
-        context.moveTo(segment.start.x, segment.start.y);
-        context.lineTo(segment.end.x, segment.end.y);
+        context.moveTo(
+          (segment.start.x + this.#offset.x) * this.#scale,
+          (segment.start.y + this.#offset.y) * this.#scale,
+        );
+        context.lineTo(
+          (segment.end.x + this.#offset.x) * this.#scale,
+          (segment.end.y + this.#offset.y) * this.#scale,
+        );
         context.stroke();
       } else if (segment instanceof CircularTrackSegment) {
         context.beginPath();
         context.arc(
-          segment.center.x,
-          segment.center.y,
-          segment.radius,
+          (segment.center.x + this.#offset.x) * this.#scale,
+          (segment.center.y + this.#offset.y) * this.#scale,
+          segment.radius * this.#scale,
           segment.initialAngle,
           segment.finalAngle,
           segment.counterClockWise,
@@ -89,7 +109,13 @@ class Map {
       train.update();
       context.fillStyle = "rgba(200, 0, 0, 0.8)";
       context.beginPath();
-      context.arc(train.position.x, train.position.y, 4, 0, Math.PI * 2);
+      context.arc(
+        (train.position.x + this.#offset.x) * this.#scale,
+        (train.position.y + this.#offset.y) * this.#scale,
+        4,
+        0,
+        Math.PI * 2,
+      );
       context.fill();
     });
 
