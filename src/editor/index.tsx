8a6651f361e96a5d35b56eab42d9d042b13bfa00
Joe <joe.cardwell@gmail.com>
2023-09-10 20:41:42 -0400

    Add React

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,5 +1,10 @@
 import TrackEditor from "./TrackEditor";
 import { build as buildComplex } from "../engine/networks/TestingNetworks/DisconnectedLinear";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = createRoot(document.querySelector<HTMLDivElement>("#root")!);
+container.render(<h1>aerfwse</h1>);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div id="editor-holder">
