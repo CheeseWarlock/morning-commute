3201a2b5e55dc373caa90fe1dda2389d09c974ae
Joe <joe.cardwell@gmail.com>
2023-09-15 17:00:12 -0400

    Streamline editor position updates

--- src/editor/TrackSegmentDetail.tsx
+++ src/editor/TrackSegmentDetail.tsx
@@ -3,63 +3,59 @@ import TrackSegment from "../engine/TrackSegment";
 import CircularTrackSegment from "../engine/CircularTrackSegment";
 import Network from "../engine/Network";
 import { findCenter } from "./utils";
+import Point from "../engine/Point";
+import LinearTrackSegment from "../engine/LinearTrackSegment";
 
 const TrackSegmentDetail = (props: { network: Network, segmentIndex: number, update: React.Dispatch<React.SetStateAction<Network>> }) => {
   const { update } = props;
-  const doUpdateSX : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const val = ev.target.value;
-    const network = props.network;
-    const aaaaa = new Network(network.segments);
-    aaaaa.segments[props.segmentIndex].start.x = Number.parseInt(val);
-    update(aaaaa);
-  }
 
-  const doUpdateSY : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const val = ev.target.value;
+  const doUpdateProp = (newProp: { start?: Partial<Point>, end?: Partial<Point>, theta?: number, counterClockWise?: boolean }) => {
     const network = props.network;
-    const aaaaa = new Network(network.segments);
-    aaaaa.segments[props.segmentIndex].start.y = Number.parseInt(val);
-    update(aaaaa);
-  }
 
-  const doUpdateEX : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const val = ev.target.value;
-    const network = props.network;
-    const aaaaa = new Network(network.segments);
-    aaaaa.segments[props.segmentIndex].end.x = Number.parseInt(val);
-    update(aaaaa);
-  }
-
-  const doUpdateEY : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const val = ev.target.value;
-    const network = props.network;
-    const aaaaa = new Network(network.segments);
-    aaaaa.segments[props.segmentIndex].end.y = Number.parseInt(val);
-    update(aaaaa);
-  }
-
-  const doUpdateTheta : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const val = ev.target.value;
-    const network = props.network;
-    const aaaaa = new Network(network.segments);
-    const thisSegment = (aaaaa.segments[props.segmentIndex] as CircularTrackSegment);
-    const newPos = findCenter(thisSegment.start,thisSegment.end,Number.parseFloat(val),thisSegment.counterClockWise);
-    thisSegment.center.x = newPos.x;
-    thisSegment.center.y = newPos.y;
-    update(aaaaa);
-  }
-
-  const doUpdateCCW : React.ChangeEventHandler<HTMLInputElement> = (ev) => {
-    const boolVal = ev.target.checked;
-    const network = props.network;
-    const aaaaa = new Network(network.segments);
-    const thisSegment = (aaaaa.segments[props.segmentIndex] as CircularTrackSegment);
-    const newPos = findCenter(thisSegment.start,thisSegment.end,thisSegment.theta,boolVal);
-    thisSegment.center.x = newPos.x;
-    thisSegment.center.y = newPos.y;
-    
-    thisSegment.counterClockWise = boolVal;
-    update(aaaaa);
+    if (network.segments[props.segmentIndex] instanceof CircularTrackSegment) {
+      const newNetwork = new Network(network.segments);
+      const thisSegment = (newNetwork.segments[props.segmentIndex] as CircularTrackSegment);
+      const newPos = findCenter(
+        { ...thisSegment.start, ...newProp.start },
+        { ...thisSegment.end, ...newProp.end },
+        (newProp.theta ?? thisSegment.theta),
+        (newProp.counterClockWise ?? thisSegment.counterClockWise)
+      );
+      if (newProp.start?.x != null) {
+        thisSegment.start.x = newProp.start.x;
+      }
+      if (newProp.start?.y != null) {
+        thisSegment.start.y = newProp.start.y;
+      }
+      if (newProp.end?.x != null) {
+        thisSegment.end.x = newProp.end.x;
+      }
+      if (newProp.end?.y != null) {
+        thisSegment.end.y = newProp.end.y;
+      }
+      if (newProp.counterClockWise != null) {
+        thisSegment.counterClockWise = newProp.counterClockWise;
+      }
+      thisSegment.center.x = newPos.x;
+      thisSegment.center.y = newPos.y;
+      update(newNetwork);
+    } else {
+      const newNetwork = new Network(network.segments);
+      const thisSegment = (newNetwork.segments[props.segmentIndex] as LinearTrackSegment);
+      if (newProp.start?.x != null) {
+        thisSegment.start.x = newProp.start.x;
+      }
+      if (newProp.start?.y != null) {
+        thisSegment.start.y = newProp.start.y;
+      }
+      if (newProp.end?.x != null) {
+        thisSegment.end.x = newProp.end.x;
+      }
+      if (newProp.end?.y != null) {
+        thisSegment.end.y = newProp.end.y;
+      }
+      update(newNetwork);
+    }
   }
 
   const segment = props.network.segments[props.segmentIndex];
@@ -69,23 +65,23 @@ const TrackSegmentDetail = (props: { network: Network, segmentIndex: number, upd
     <h4>From</h4>
     <p>
       <span>X</span>
-      <input type="number" value={segment.start.x} onChange={doUpdateSX}></input>
+      <input type="number" value={segment.start.x} onChange={(ev) => doUpdateProp({ start: { x: Number.parseInt(ev.target.value)}})}></input>
       <span>Y</span>
-      <input type="number" value={segment.start.y} onChange={doUpdateSY}></input>
+      <input type="number" value={segment.start.y} onChange={(ev) => doUpdateProp({ start: { y: Number.parseInt(ev.target.value)}})}></input>
     </p>
     <h4>To</h4>
     <p>
       <span>X</span>
-      <input type="number" value={segment.end.x} onChange={doUpdateEX}></input>
+      <input type="number" value={segment.end.x} onChange={(ev) => doUpdateProp({ end: { x: Number.parseInt(ev.target.value)}})}></input>
       <span>Y</span>
-      <input type="number" value={segment.end.y} onChange={doUpdateEY}></input>
+      <input type="number" value={segment.end.y} onChange={(ev) => doUpdateProp({ end: { y: Number.parseInt(ev.target.value)}})}></input>
     </p>
     {segment instanceof CircularTrackSegment && <>
       <h4>Angle</h4>
       <p>
-      <input type="number" value={segment.theta} step="any" onChange={doUpdateTheta}></input>
+      <input type="number" value={segment.theta} step="any" onChange={(ev) => doUpdateProp({ theta:Number.parseInt(ev.target.value)})}></input>
       <span>CCW</span>
-      <input type="checkbox" checked={segment.counterClockWise} onChange={doUpdateCCW}></input>
+      <input type="checkbox" checked={segment.counterClockWise} onChange={(ev) => doUpdateProp({ counterClockWise: ev.target.checked})}></input>
     </p>
       </>
     }
