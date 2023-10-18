b7944859c1fc790fa969a8375a1624730d252f54
Joe <joe.cardwell@gmail.com>
2023-10-18 16:00:48 -0400

    Start adding Tailwind styling to editor

--- src/editor/TrackSegmentDetail.tsx
+++ src/editor/TrackSegmentDetail.tsx
@@ -59,37 +59,48 @@ const TrackSegmentDetail = (props: { network: Network, segmentIndex: number, upd
 
   const segment = props.network.segments[props.segmentIndex];
 
-  return <div className="segment-info">
-    <h3>{segment instanceof CircularTrackSegment ? "Circular" : "Linear"}</h3>
-    {segment.stations.length &&
+  return <div className="flex flex-col m-4">
+    <h3 className="text-xl">{segment instanceof CircularTrackSegment ? "Circular" : "Linear"} Segment
+      <span className="font-mono"> B2EE491</span>
+    </h3>
+    {segment.stations.length > 0 &&
     <>
-      <h4>Stations</h4>
+      <h4 className="text-lg">Stations</h4>
       <span>{segment.stations.length}</span>
       </>
     }
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <h4>From</h4>
-    <p>
-      <span>X</span>
-      <input type="number" value={segment.start.x} onChange={(ev) => doUpdateProp({ start: { x: Number.parseInt(ev.target.value)}})}></input>
-      <span>Y</span>
-      <input type="number" value={segment.start.y} onChange={(ev) => doUpdateProp({ start: { y: Number.parseInt(ev.target.value)}})}></input>
+    <h4 className="text-lg">From</h4>
+    <div className="my-3 flex flex-row">
+      <div className="font-mono border border-slate-300 p-2 m-2 w-1/3 flex flex-row">
+        <label className="pr-2">X</label>
+        <input type="number" className="w-full border-slate-300 background-slate-50" value={segment.start.x} onChange={(ev) => doUpdateProp({ start: { x: Number.parseInt(ev.target.value)}})}></input>
+      </div>
+      <span className="font-mono border border-slate-300 p-2 m-2 w-1/3 flex flex-row">
+        <label className="pr-2">Y</label>
+        <input type="number" value={segment.start.y} onChange={(ev) => doUpdateProp({ start: { y: Number.parseInt(ev.target.value)}})}></input>
+      </span>
+    </div>
+    <h4 className="text-lg">To</h4>
+    <p className="my-3">
+      <span className="font-mono border border-slate-300 p-2 m-2">
+        <label className="pr-2">X</label>
+        <input type="number" value={segment.end.x} onChange={(ev) => doUpdateProp({ end: { x: Number.parseInt(ev.target.value)}})}></input>
+      </span>
+      <span className="font-mono border border-slate-300 p-2 m-2">
+        <label className="pr-2">Y</label>
+        <input type="number" value={segment.end.y} onChange={(ev) => doUpdateProp({ end: { y: Number.parseInt(ev.target.value)}})}></input>
+      </span>
     </p>
-    <h4>To</h4>
-    <p>
-      <span>X</span>
-      <input type="number" value={segment.end.x} onChange={(ev) => doUpdateProp({ end: { x: Number.parseInt(ev.target.value)}})}></input>
-      <span>Y</span>
-      <input type="number" value={segment.end.y} onChange={(ev) => doUpdateProp({ end: { y: Number.parseInt(ev.target.value)}})}></input>
-    </p>
+    
     {segment instanceof CircularTrackSegment && <>
-      <h4>Angle</h4>
-      <p>
-      <input type="number" value={segment.theta} step="any" onChange={(ev) => doUpdateProp({ theta:Number.parseInt(ev.target.value)})}></input>
-      <span>CCW</span>
-      <input type="checkbox" checked={segment.counterClockWise} onChange={(ev) => doUpdateProp({ counterClockWise: ev.target.checked})}></input>
+      <h4 className="text-lg">Angle</h4>
+      <p className="my-3">
+        <span className="font-mono border border-slate-300 p-2 m-2">
+          <input type="number" value={segment.theta} step="any" onChange={(ev) => doUpdateProp({ theta:Number.parseInt(ev.target.value)})}></input>
+        </span>
+        
+        <span>CCW</span>
+        <input type="checkbox" checked={segment.counterClockWise} onChange={(ev) => doUpdateProp({ counterClockWise: ev.target.checked})}></input>
     </p>
       </>
     }
